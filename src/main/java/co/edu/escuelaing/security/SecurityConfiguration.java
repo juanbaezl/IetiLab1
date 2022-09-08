bdc93ce8052f687ddd8b6774958a093c161a5ae9
juanbaezl <60081580+juanbaezl@users.noreply.github.com>
2022-09-08 17:07:54 -0500

    security

--- src/main/java/co/edu/escuelaing/security/SecurityConfiguration.java
+++ src/main/java/co/edu/escuelaing/security/SecurityConfiguration.java
@@ -1,26 +1,43 @@
-package co.edu.escuelaing.config;
+package co.edu.escuelaing.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import edu.eci.ieti.config.JwtRequestFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfiguration
         extends WebSecurityConfigurerAdapter {
 
+    JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfiguration(@Autowired JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Override
     protected void configure(HttpSecurity http)
             throws Exception {
-        http.cors().and().csrf().disable()
+        http.addFilterBefore(jwtRequestFilter,
+                BasicAuthenticationFilter.class)
+                .cors()
+                .and()
+                .csrf()
+                .disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/v2/users").permitAll()
-                .antMatchers(HttpMethod.POST,"/v1/auth" ).permitAll()
+                .antMatchers(HttpMethod.POST, "/v2/auth")
+                .permitAll().antMatchers(HttpMethod.POST, "/api/v2/customers")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
